ca3cdfd00e71b3c7d34eb70f1b30ee75ff2a6519
Banker <apexhomebuilderllc@gmail.com>
2025-03-17 19:37:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     let body = document.querySelector("body");
+
+    // Falling Money Animation (Homepage)
     if (body.classList.contains("homepage")) {
         setInterval(() => {
             let money = document.createElement("img");
             money.src = "images/A-money.png";
-            money.className = "falling-money";
+            money.className = "falling-object";
             money.style.left = Math.random() * window.innerWidth + "px";
             document.body.appendChild(money);
             setTimeout(() => money.remove(), 3000);
+        }, 500);
+    }
+
+    // Falling Coins Animation (Airdrop Signup Page)
+    if (body.classList.contains("airdrop-signup")) {
+        setInterval(() => {
+            let coin = document.createElement("img");
+            coin.src = "images/O-coin.png"; // Image for falling coins
+            coin.className = "falling-object";
+            coin.style.left = Math.random() * window.innerWidth + "px";
+            document.body.appendChild(coin);
+            setTimeout(() => coin.remove(), 3000);
         }, 500);
     }
 });
